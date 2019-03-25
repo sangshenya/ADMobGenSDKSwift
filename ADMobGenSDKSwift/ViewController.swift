fed7f2354fd3d0ea3b8d0458ae7931f914291d92
sangshen <sangshen@ecook.cn>
2019-03-25 10:23:35 +0800

    Initial Commit

---
+++ ADMobGenSDKSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ADMobGenSDKSwift
+//
+//  Created by 陈坤 on 2019/3/25.
+//  Copyright © 2019 陈坤. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
